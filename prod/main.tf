4b4e77f35d8051eaa67d8813249f5cd7d6bc8f09
motatoes <moe.habib9@gmail.com>
2025-06-11 16:37:12 -0700

    initial commit

---
+++ prod/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # Replace with your desired AWS region
+}
+
+resource "aws_ssm_parameter" "example_secret" {
+  name        = "/prod/example/secret/"
+  description = "Example secret parameter"
+  type        = "SecureString"
+  value       = "dummy-secret-value"
+
+  tags = {
+    Environment = "prod"
+  }
+}
